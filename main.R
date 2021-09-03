75629e9a9a9e9b455c45f3be792515caf98a3a45
lucaslelann <lucas.lelann@gmail.com>
2021-09-03 13:50:59 +0200

    Push the workflow changes

--- main.R
+++ main.R
@@ -1,7 +1,9 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
+ctx <- tercenCtx() # Get data from the data step
+
+ctx   %>% 
   select(.y, .ci, .ri) %>% 
   group_by(.ci, .ri) %>%
   summarise(median = median(.y)) %>%
